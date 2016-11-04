316bcc5b16c5003bc58bdee7fb045c7100f67310
Eric Milan <emilan@etmilan.org>
2016-11-04 13:30:44 -0400

    Finished Chapter 5

--- Chapter_5/add_inventory.py
+++ Chapter_5/add_inventory.py
@@ -1,5 +1,5 @@
 def display_inventory(inventory):
-    print('Inventory:')
+    print('\nInventory:')
     item_total = 0
     for k, v in inventory.items():
         print(str(v) + ' ' + k)
@@ -7,9 +7,15 @@ def display_inventory(inventory):
     print('Total number of items: ' + str(item_total))
 
 def add_to_inventory(inventory, added_items):
-    
-
+    for i in added_items:
+        if i in inventory.keys():
+            inventory[i] += 1 
+        else:
+            inventory.setdefault(i, 1)
+    return inventory
+        
 inv = {'gold coin': 42, 'rope': 1}
+display_inventory(inv)
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = add_to_inventory(inv, dragon_loot)
 
