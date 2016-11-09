ff0a97628d95251664a4082570acaef7eb3cfc04
Eric Milan <emilan@etmilan.org>
2016-11-09 12:30:13 -0500

    Chapter 7

---
+++ Chapter_7/strong_password_dectection.py
@@ -0,0 +1,20 @@
+#! python3.5
+# strong_password_detection.py - Dectects if user supplied password is strong or weak
+
+import re
+
+def passwd_stregth(password):
+    passwd_length_regex = re.compile(r'''^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+${8,8}''')
+
+    if passwd_length_regex.search(password) is None:
+        return False
+    else:
+        return True
+
+print('Enter a password')
+passwd = input()
+pass_stregth = passwd_stregth(passwd)
+if pass_stregth == True:
+    print('Your password is strong')
+else:
+    print('Your password is weak')
