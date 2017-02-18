c1449c151cfcaba9c0ca1d3c7f74c1580508f205
Eric Milan <emilan@etmilan.org>
2017-02-18 09:48:48 -0500

    Finished chapter 7 :)

--- Chapter_7/regex_strip.py
+++ Chapter_7/regex_strip.py
@@ -5,12 +5,14 @@ import re
 
 def strip(string, char_to_strip):
     if char_to_strip == '':
-        strip_space = re.compile(r'''(\w+.+)\w''')
+        strip_space = re.compile(r'''(\w.+)\S''')
         new_string = strip_space.search(string)
         return new_string.group(0)
     else:
-        char_strip = re.compile(r':wq
-                ')
+        char_strip = re.compile(r'[^{0}](.*)[^{0}]'.format(char_to_strip))
+        #char_strip = re.compile(r'[^0](.*)[^0]')
+        new_string = char_strip.search(string)
+        return new_string.group(0)
 
 
 print('Enter a string')
@@ -19,4 +21,5 @@ print('Enter a character to strip from the string (Hit enter for space)')
 char_to_strip = input()
 
 new_string = strip(string, char_to_strip)
+print("\n")
 print(new_string)
