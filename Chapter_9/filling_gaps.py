649427ccd76096e0e45c60c5068dc43825753ea0
Eric Milan <emilan@etmilan.org>
2017-06-11 10:11:08 -0400

    Finally got the filling gaps program working. Probably could be cleaned up at some point

--- Chapter_9/filling_gaps.py
+++ Chapter_9/filling_gaps.py
@@ -27,4 +27,24 @@ regex_file_list.sort()
 # check if there are gaps in list order
 for file_name in regex_file_list:
     m = file_regex.match(file_name)
-    print(m.group(0))
+    if hun_count > 9:
+        hun_count = 0
+    if ten_count > 9:
+        ten_count = 0
+    if single_count > 9:
+        single_count = 0
+
+    for hun in m.group(1):
+        if hun_count != int(hun):
+            hun = hun_count
+            hun_count += 1
+        for ten in m.group(2):
+            if ten_count != int(ten):
+                ten = ten_count
+                ten_count += 1
+            for single in m.group(3):
+                if single_count != int(single):
+                    single = single_count
+                single_count += 1
+
+    shutil.copy('spam/' + m.group(0), 'spam_test/' + 'spam' + str(hun) + str(ten) + str(single) + '.txt')
