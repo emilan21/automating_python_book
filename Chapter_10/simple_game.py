de4266657ee87ccb1b9aabe5ecab3b1054f1b462
Eric Milan <emilan@etmilan.org>
2017-06-11 19:25:21 -0400

    Done with chapter 10

--- Chapter_10/simple_game.py
+++ Chapter_10/simple_game.py
@@ -2,20 +2,28 @@
 
 import random
 
+def check_guess(guess):
+    if guess == 'tails':
+        return 0
+    elif guess == 'heads':
+        return 1
+
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 toss = random.randint(0, 1)     # 0 is tails, 1 is heads
-if guess == 'tails':
-    guess = 0
-elif guess == 'heads':
-    guess = 1
+
+guess = check_guess(guess)
+
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
     guess = input()
+
+    guess = check_guess(guess)
+
     if toss == guess:
         print('You got it!')
     else:
